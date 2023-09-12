b6b4caf9c3bb132a7bc577789e4762a993929a1e
Gleb <65162943+FirstAfterGod2501@users.noreply.github.com>
2023-09-12 20:46:35 +0300

    add task2

--- task2/main.py
+++ task2/main.py
@@ -1,71 +1,38 @@
-import argparse
+import time
+
+import networkx as nx
+import requests
 import json
-import sys
-
-from packaging import version
-from packaging.requirements import Requirement
-from packaging.utils import canonicalize_name
-from packaging.pypi.simple import SimpleIndex
-
-def get_dependencies_graph(package_name):
-    graph = {}
-
-    package_info = SimpleIndex().info(package_name)
-    if not package_info:
-        print(f"Package '{package_name}' not found.")
-        return graph
-
-    def get_dependencies(package, version_spec):
-        package_key = canonicalize_name(package)
-
-        if package_key in graph:
-            return
-
-        graph[package_key] = []
-
-        try:
-            package_info = SimpleIndex().info(package)
-            requires_dist = package_info['info']['requires_dist']
-        except:
-            return
-
-        for requirement in requires_dist:
-            req = Requirement(requirement)
-            dep_package = canonicalize_name(req.name)
-
-            if dep_package not in graph[package_key]:
-                graph[package_key].append(dep_package)
-
-            if version_spec:
-                for available_version in SimpleIndex().iter_versions(dep_package):
-                    if version_spec.contains(version.parse(available_version)):
-                        get_dependencies(dep_package, f"=={available_version}")
-            else:
-                get_dependencies(dep_package, req.specifier)
-
-    get_dependencies(package_info["name"], "")
-
-    return graph
 
 
-def generate_graphviz(graph):
-    graphviz = "digraph Dependencies {\n"
+# https://pypi.org/pypi/pdfplumber/json
 
-    for package, dependencies in graph.items():
-        graphviz += f'\t"{package}" [label="{package}"];\n'
-        for dependency in dependencies:
-            graphviz += f'\t"{package}" -> "{dependency}";\n'
+def getPackage(name, sep, deps):
+    try:
+        result = requests.get(f'https://pypi.org/pypi/{name}/json')
+    except  Exception as e:
+        normal = "true"
+        return
+    if ("info" in result.json() and "requires_dist" in result.json()["info"] and result.json()["info"][
+        "requires_dist"]):
+        for i in result.json()["info"]["requires_dist"]:
+            pack = str(i).split()[0].split(">", 1)[0].split("=", 1)[0].split("<", 1)[0].split("[", 1)[0].split(';', 1)[0].split('~', 1)[0]
+            if deps and pack not in deps:
+                #print(deps)
+                print(sep + " " + pack)
+                deps.append(pack)
+                newDeps = deps
+                getPackage(pack, sep + pack + "->", newDeps)
 
-    graphviz += "}\n"
-    return graphviz
+            if not deps:
+                print(sep + " " + pack)
+                deps.append(pack)
+                newDeps = deps
+                getPackage(pack, sep + pack + "->", newDeps)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Dependency graph generator.")
-    parser.add_argument("package", help="Package name")
-    args = parser.parse_args()
+    dependency_graph = nx.DiGraph()
+    getPackage("tensorflow", "tensorflow->", [])
 
-    dependencies_graph = get_dependencies_graph(args.package)
-    graphviz = generate_graphviz(dependencies_graph)
-
-    print(graphviz)
+    # print(graphviz)
